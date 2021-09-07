a4204f4a5923205c6327feb2be73e64571802df5
johnwesely <johnpwesely@gmail.com>
2021-09-07 09:37:17 -0600

    Add files via upload

--- GenericSort.c
+++ GenericSort.c
@@ -1,23 +1,12 @@
+/* Implementation of mergeSort that operates on generic data types
+** challenge problem 14 from Modern C
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
 #include <assert.h>
-
-void printBits(size_t const size, void const * const ptr)
-{
-    unsigned char *b = (unsigned char*) ptr;
-    unsigned char byte;
-    int i, j;
-    
-    for (i = size-1; i >= 0; i--) {
-        for (j = 7; j >= 0; j--) {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
-    }
-    puts("");
-}
 
 typedef int compare_function(void const*, void const*);
 
@@ -36,7 +25,7 @@ void genericSort(void* base, size_t n,
 /* generic merge helper function */
 void merge(void* base, size_t l, size_t m, size_t r,
            size_t size, compare_function* compar);
-/* generic merge sort function */
+/* generic mergeSort function */
 void mergeSort(void* base, size_t l, size_t r,
                size_t size, compare_function* compar);
 /* compare functions */
@@ -55,20 +44,16 @@ int main(void) {
 }
 
 void genericSort(void* base, size_t n, size_t size, compare_function* compar) {
-    printf("genericSort\n");
     mergeSort(base, 0, n * size - 1, size, compar);
 }
 
 void mergeSort(void* base, size_t l, size_t r, size_t size, compare_function* compar) {
-    printf("mergeSort\n");
-    printf("l = %zu, r = %zu\n", l, r);
     //base case
     if (r - l > size - 1) {
         // determine middle index of generic array
         size_t typeIndexL = l / size;
         size_t typeIndexR = (r + 1) / size;
         size_t m = (typeIndexL + (typeIndexR - typeIndexL) / 2) * size;
-        printf("m = %zu\n", m);
 
         // sort left and right halves of sub array
         if (r - l > size * 2 - 1) {
@@ -82,12 +67,9 @@ void mergeSort(void* base, size_t l, size_t r, size_t size, compare_function* co
 }
 
 void merge(void* base, size_t l, size_t m, size_t r, size_t size, compare_function* compar) {
-    printf("merge\n");
-    printf("l = %zu, m = %zu, r = %zu, size = %zu\n", l, m, r, size);
     // calculate size of temp arrays
     size_t nL = m - l;
     size_t nR = r - m + 1;
-    printf("nL = %zu, nR = %zu\n", nL, nR);
 
     // sub array index iterators 
     size_t mergeIndex = l, leftIndex = 0, rightIndex = 0;
@@ -95,6 +77,7 @@ void merge(void* base, size_t l, size_t m, size_t r, size_t size, compare_functi
     // initialize and populate temp arrays
     uint8_t leftArr[nL];
     uint8_t rightArr[nR];
+    // cast void* base as array of unint8_t for bytewise manipulation 
     uint8_t* baseArr = ((uint8_t*) base);
 
     for (size_t i = 0; i < nL; ++i) {
@@ -104,9 +87,12 @@ void merge(void* base, size_t l, size_t m, size_t r, size_t size, compare_functi
         rightArr[i] = baseArr[m + i];
     }
     
-    // merge left and right arrays in order 
+    // merge left and right arrays into base array in order
     while (leftIndex < nL && rightIndex < nR) {
+        // pass pointers to current index of both arrays to 
+        // given compare function
         if (compar(&leftArr[leftIndex], &rightArr[rightIndex]) < 1) {
+            // copy size bytes from lesser value to base array
             for (size_t i = 0; i < size; ++i) {
                 baseArr[mergeIndex] = leftArr[leftIndex];
                 ++mergeIndex;
@@ -122,6 +108,7 @@ void merge(void* base, size_t l, size_t m, size_t r, size_t size, compare_functi
         }    
     }
 
+    // insert remaining values in to base array
     while (leftIndex < nL) {
         baseArr[mergeIndex] = leftArr[leftIndex];
         ++mergeIndex;
@@ -145,34 +132,18 @@ int compare_unsigned(void const* a, void const* b) {
 }
 
 int compare_int(void const* a, void const* b) {
-    printf("compare_int: ");
     int const* A = a;
     int const* B = b;
-    printf("*A = %d, *B = %d\n", *A, *B);
     if (*A < *B) return -1;
     else if (*A > *B) return +1;
     else return 0;
 }
 
 int compare_string(void const* a, void const* b) {
-    uint8_t arrA[sizeof(char*)];
-    uint8_t arrB[sizeof(char*)];
-    
-    char* arra = (char*) a;
-    char* arrb = (char*) b;
-    
-    for (size_t i = 0; i < sizeof(char*); ++i) {
-        arrA[i] = arra[i];
-    }
-    for (size_t i = 0; i < sizeof(char*); ++i) {
-        arrA[i] = arrb[i];
-    }
+    // casts a and b to pointers to arrays of char*
+    char* const* ptrA = a;
+    char* const* ptrB = b;
 
-    char* strA = (char*) arrA;
-    char* strB = (char*) arrB;
-
-    int ret = strcmp(strA, strB);
-    printf("compare_string: *A = %s, *B = %s, ret = %d\n", strA, strB, ret);
-    return ret;
+    return strcmp(*ptrA, *ptrB);
 }
 
